9a71c575659188902730ea38ba8f29360f668389
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-09-01 09:18:24 +0200

    service worker + notifications

---
+++ src/js/app.js
@@ -0,0 +1,66 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('../../sw.js')
+      .then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+    //https://developers.google.com/web/fundamentals/primers/service-workers
+    
+    let deferredPrompt;
+    
+    window.addEventListener('beforeinstallprompt', (event) => {
+      // Prevent the mini-infobar from appearing on mobile
+      event.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = event;
+  
+      document.querySelector(".install-offline").classList.toggle("hidden", false);
+    });
+  
+    document.querySelector(".install-offline").addEventListener('click', () => {
+      document.querySelector(".install-offline").classList.add("hidden");
+      // Show the install prompt
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+      });
+      
+    });
+  }  
+  
+  // Notification
+  Notification.requestPermission(function(status){
+    console.log("Notification premission status: ", status)
+    if(Notification.permission === "granted"){
+      navigator.serviceWorker.getRegistration()
+      .then(function(reg){
+        let options = {
+          body: "You can change the main color and block the news articles you have no interest in",
+          icon: "./assets/images/news-icon-white.png",
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          },
+          actions: [
+            {action: 'Settings', title: 'To change color or hide news stories click here',
+              icon: './assets/images/notifications/check.png'},
+            {action: 'close', title: 'Close notification',
+              icon: './assets/images/notifications/x.png'},
+          ]
+        }
+        reg.showNotification("Change main color", options);
+      });
+    }
+  });
+  
+  
---
+++ src/sw.js
@@ -0,0 +1,66 @@
+const cacheName = `static-cache-v8`;
+const filesToCache = [
+    `./`,
+    `./offline.html`,
+    `./assets/css/all-sites.css`,
+    `./assets/css/index/index.css`,
+    `./assets/images/news-icon192.png`,
+    `./assets/images/news-icon512.png`,
+    `./settings.html`,
+    `./assets/css/settings/settings.css`
+];
+
+self.addEventListener('install', function(event) {
+    console.log('Service Worker Installed');
+    event.waitUntil(
+      caches.open(cacheName)
+      .then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        cache.addAll(filesToCache);
+      }) 
+    );
+});
+
+self.addEventListener(`activate`, function(event){
+    console.log("activated ", event)
+    event.waitUntil(
+        caches.keys()
+        .then(function(keys){
+            console.log(keys);
+            return Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))
+        })
+    );
+});
+
+self.addEventListener(`fetch`, function(event){
+    console.log(`fetch `, event);  
+    event.respondWith(   
+        caches.open(cacheName)
+        .then(function(cache){
+            return cache.match(event.request)
+            .then(function(response){
+                return(
+                    response || fetch(event.request)
+                    .then(function(response){
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                );
+            })
+            .catch(function(){
+                return caches.match(`./offline.html`); 
+            });
+        })
+    );
+});
+self.addEventListener('notificationclick', function (event) {
+    // clients.openWindow("/");
+    if(event.action === "Settings"){
+        console.log(event.action)
+        clients.openWindow("/settings.html")
+    }else{
+        event.notification.close();
+    }
+});
+
+//https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker#using_the_cache_api_in_the_service_worker
