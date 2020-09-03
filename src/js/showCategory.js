ebc6f9c25bc25dffc6e97f8f1d39e32ed51c88da
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-09-03 14:59:52 +0200

    settings done

--- src/js/showCategory.js
+++ src/js/showCategory.js
@@ -1,3 +1,51 @@
+let artChecking = localStorage.getItem("category-arts");
+let homeChecking = localStorage.getItem("category-home");
+let scienceChecking = localStorage.getItem("category-science");
+let usChecking = localStorage.getItem("category-us");
+let worldChecking = localStorage.getItem("category-world");
+
+let artChange = document.querySelector("#category-arts .fas");
+let homeChange = document.querySelector("#category-home .fas");
+let scienceChange = document.querySelector("#category-science .fas");
+let usChange = document.querySelector("#category-us .fas");
+let worldChange = document.querySelector("#category-world .fas");
+
+if(artChecking == "true" || artChecking == null || artChecking == ""){
+    artChange.classList.add("fa-check");
+    artChange.classList.remove("fa-times");
+}else{
+    artChange.classList.add("fa-times");
+    artChange.classList.remove("fa-check");
+}
+if(homeChecking == "true" || homeChecking == null || artChecking == ""){
+    homeChange.classList.add("fa-check");
+    homeChange.classList.remove("fa-times");
+}else{
+    homeChange.classList.add("fa-times");
+    homeChange.classList.remove("fa-check")
+}
+if(scienceChecking == "true" || scienceChecking == null || scienceChecking == ""){
+    scienceChange.classList.add("fa-check");
+    scienceChange.classList.remove("fa-times");
+}else{
+    scienceChange.classList.add("fa-times");
+    scienceChange.classList.remove("fa-check")
+}
+if(usChecking == "true" || usChecking == null || usChecking == ""){
+    usChange.classList.add("fa-check");
+    usChange.classList.remove("fa-times");
+}else{
+    usChange.classList.add("fa-times");
+    usChange.classList.remove("fa-check");
+}
+if(worldChecking === "true" || worldChecking == null || worldChecking == ""){
+    worldChange.classList.add("fa-check");
+    worldChange.classList.remove("fa-times");
+}else{
+    worldChange.classList.add("fa-times");
+    worldChange.classList.remove("fa-check");
+}
+
 let aChosen = document.querySelector("#category-arts");
 let hChosen = document.querySelector("#category-home");
 let sChosen = document.querySelector("#category-science");
